137a3fb3afa9593bd668500de39daef5542b9fc1
henriqueif <henriquepke69@gmail.com>
2020-11-28 20:49:09 -0300

    Adiciona solução tomadas.py

---
+++ Tomadas.py
@@ -0,0 +1,11 @@
+T_1, T_2, T_3, T_4 = input().split(" ")
+
+T_1 = int(T_1)
+T_2 = int(T_2)
+T_3 = int(T_3)
+T_4 = int(T_4)
+
+if (2<=T_1<=6 and 2<=T_2<=6 and 2<=T_3<=6 and 2<=T_4<=6):
+    nT = (T_1 - 1) + (T_2 - 1) + (T_3 - 1) + (T_4) 
+
+print (nT)
